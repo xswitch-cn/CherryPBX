d69453253af9bdb1cca4fc2eca068e25ca811e53
zhuoruxue <zhuoruxue@x-y-t.cn>
2026-04-13 15:24:39 +0800

    add translation & delete call & fix position

--- app/[locale]/users/users-columns.tsx
+++ app/[locale]/users/users-columns.tsx
@@ -87,7 +87,7 @@ export function createUserColumns({ t, tt, onDelete }: CreateUserColumnsProps): 
                   window.location.href = `/users/${user.id}`;
                 }}
               >
-                {tt("Details")}
+                {tt("details")}
               </DropdownMenuItem>
               <DropdownMenuSeparator />
               <DropdownMenuItem
